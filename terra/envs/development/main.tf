b1b151eabaf8e831401f28d4f6f4cb1ef5028eb4
iamvijayaragavan <success.vijayaragavan@gmail.com>
2020-07-03 20:05:08 +0530

    Updated main.tf file

---
+++ terra/envs/development/main.tf
@@ -0,0 +1,26 @@
+module "resource_group_demoaks" {
+    source                = "../../modules/resource_groups"
+    resource_group_name   = var.resource_group_name
+    location              = var.location
+    tags                  = var.tags
+}
+
+
+module "acr_registry_demo" {
+    source                = "../../modules/aks_registry"
+    name                  =  var.acr_name
+    resource_group_name   = var.resource_group_name
+    location              = var.location
+    tags                  = var.tags
+}
+
+module "aks_cluster_demo" {
+    source                = "../../modules/aks_cluster"
+    name                  =  var.aks_name
+    resource_group_name   = var.resource_group_name
+    location              = var.location
+    tags                  = var.tags
+    dns_prefix            = var.dns_prefix
+    ssh_admin_name        = var.ssh_admin_name
+    ssh_key               = var.ssh_key
+}
--- terra/modules/acr_registry/main.tf
+++ terra/modules/acr_registry/main.tf
@@ -4,8 +4,8 @@ data "azurerm_resource_group" current {
 
 resource "azurerm_container_registry" "acr" {
   name                     = var.acr_name
-  resource_group_name      = data.azurerm_resource_group.current.location
-  location                 = data.azurerm_resource_group.current.name
+  resource_group_name      = data.azurerm_resource_group.current.name
+  location                 = data.azurerm_resource_group.current.location
   sku                      = var.sku_type
   admin_enabled            = false
   georeplication_locations = ["East US", "West Europe"]
